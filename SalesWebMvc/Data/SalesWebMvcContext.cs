8b4c41a7a81e7fb67bbed2bfacac7a65f23ece8c
Davi Afonso <daviafonso5894@gmail.com>
2024-01-10 15:00:08 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Data
 {
     public class SalesWebMvcContext : DbContext
     {
-        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; } = default!;
+        public DbSet<Models.Department> Department { get; set; } = default!;
+        public DbSet<Models.Seller> Seller { get; set; } = default!;
+        public DbSet<Models.SalesRecord> SalesRecord { get; set; } = default!;
     }
 }

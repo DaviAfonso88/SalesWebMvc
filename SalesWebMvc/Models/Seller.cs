24a83da03673b5d5d8084f391188600ffe670694
Davi Afonso <daviafonso5894@gmail.com>
2024-01-31 14:26:46 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -11,7 +11,7 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
-        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
